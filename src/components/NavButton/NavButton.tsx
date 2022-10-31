f2fa77975e2db368f4ea244a5543ea98b9be69ab
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-10-31 21:53:01 +0300

    Nav panel and buttons added

---
+++ src/components/NavButton/NavButton.tsx
@@ -0,0 +1,26 @@
+import {useAppDispatch, useAppSelector} from "../../utils/hooks/redux-hooks";
+import {setFilter} from "../../store/slices/todoSlice";
+import {Filter, NavButtonProps} from "../../types/types";
+import {Button} from "@mui/material";
+
+export const NavButton = (props: Partial<NavButtonProps>) => {
+    const {children, filterValue = Filter.All} = props
+    const { filter } = useAppSelector((state) => state.TodoReducer);
+    const dispatch = useAppDispatch();
+
+    const switchFilter = () => {
+        dispatch(setFilter(filterValue));
+    };
+
+    return (
+        <>
+            <Button
+                variant={'contained'}
+                color={(filter === filterValue) ? 'success' : 'primary'}
+                onClick={switchFilter}
+            >
+                {children}
+            </Button>
+        </>
+    )
+}
